f7cbf385109316a9cbd2e4bc867c3087eaa011b9
장진익 <jijang@mz.co.kr>
2020-07-24 13:02:05 +0900

    자바 스크립트 첫 걸음

--- scripts/main.js
+++ scripts/main.js
@@ -57,3 +57,12 @@ if (!localStorage.getItem('name')) {
 myButton.onclick = function() {
     setUserName();
 }
+
+const para = document.querySelector('p');
+
+para.addEventListener('click', updateName);
+
+function updateName() {
+    let name = prompt('Enter a new name');
+    para.textContent = 'Player 1: ' + name;
+}
